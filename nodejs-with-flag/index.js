9702201c84a56cf6caad16426f1dd3560671aec1
Edy Segura <edysegura@gmail.com>
2019-06-04 23:07:50 -0300

    Update Node.js exemple with flag

---
+++ nodejs-with-flag/index.js
@@ -0,0 +1,7 @@
+import BMIService from './bmi.service'
+
+const bodyMassIndex = BMIService
+  .getIndex(76, 1.71)
+  .toFixed(2)
+
+console.log(bodyMassIndex)
--- nodejs-with-flag/index.mjs
+++
@@ -1,7 +0,0 @@
-import BMIService from './bmi.service'
-
-const bodyMassIndex = BMIService
-  .getIndex(76, 1.71)
-  .toFixed(2)
-
-console.log(bodyMassIndex)
